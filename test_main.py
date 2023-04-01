3165f056974f4b7d16630de3697df4743e6cb73a
Losyash <alexei.naimushin@gmail.com>
2023-04-01 12:28:14 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,12 +5,14 @@ client = TestClient(app)
 
 
 def test_read_main():
+    '''Tests main route "/" availability'''
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
 def test_predict_positive():
+    '''Tests predict positive tone of the text'''
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
     json_data = response.json()
@@ -19,6 +21,7 @@ def test_predict_positive():
 
 
 def test_predict_negative():
+    '''Tests predict negative tone of the text'''
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
