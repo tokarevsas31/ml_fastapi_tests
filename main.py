ee838e8b9975705feca14e6d0133b8a3da137039
Nadezhda Kozhedub <lnc3001@gmail.com>
2023-04-01 09:41:44 +0500

    Пример

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from fastapi import FastAPI
 from transformers import pipeline
-from pydantic import BaseModel
 
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
 
-class Item(BaseModel):
-    text: str
-
-
-app = FastAPI()
-classifier = pipeline("sentiment-analysis")
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+classifier("Я обожаю инженерию машинного обучения!")
