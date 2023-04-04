450ef8de084d08f3df799235603a529f403df5ee
Artem <artemonuys@gmail.com>
2023-04-04 14:18:05 +0300

    Add streamlit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+import streamlit as st
 from transformers import pipeline
 from pydantic import BaseModel
+
+classifier = pipeline("sentiment-analysis")
+st.title("Sentiment Analysis")
+st.subheader("Enter the text you would like to analyze:")
+item = st.text_input("Input text and press the 'Enter' key")
+if len(item) > 0:
+    result = classifier(item)
+    st.write("Sentiment of Text: ", result[0]["label"])
+    st.write("Score: ", result[0]["score"])
 
 
 class Item(BaseModel):
