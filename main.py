45cd1cde751c2245464320313baa03467b02a64b
Edward <toy.owl@yandex.ru>
2023-04-14 11:08:57 +0300

    refact: added suggestions for changing the code
    1.Added the capability to interact with the model using a web interface.
    2.Implemented asynchronous request processing.
    3.Added try-except wrappers for code that can throw exceptions.
    4.Prepared warm-up steps for working with the NLP model.
    5.Added software versioning and froze dependencies.
    6.Implemented meaningful naming of HTTP response code constants as HTTP_200_OK.
    7.Used double quotation marks uniformly throughout the code.
    8.Updated README.md to reflect the changes made in the codebase.

--- main.py
+++ main.py
@@ -1,21 +1,78 @@
+import asyncio
+import logging
+import uvicorn
+
+from sys import platform
+
 from fastapi import FastAPI
-from transformers import pipeline
 from pydantic import BaseModel
 
+import torch
+from transformers import pipeline
 
+# Define constants for application
+APP_VERSION = "1.0.0"
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Define Item model
 class Item(BaseModel):
     text: str
 
+default_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+default_host = "127.0.0.1" if platform == "win32" else "0.0.0.0"
+default_port = 8000
 
+# Define FastAPI app
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
 
+# Load NLP model for sentiment analysis
+logger.info("Loading sentiment analysis model...")
+classifier = pipeline("sentiment-analysis",
+                      device=default_device,
+                      framework="pt"
+                      )
+#classifier = pipeline("sentiment-analysis", framework="pt")
+logger.info("Sentiment analysis model loaded successfully.")
 
+# Define root endpoint
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
+# Define predict endpoint
 @app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+async def predict(item: Item):
+    try:
+        logger.info("Received text for sentiment analysis: %s", item.text)
+        result = await asyncio.to_thread(classifier, item.text)
+        label = result[0]["label"]
+        score = result[0]["score"]
+        logger.info("Sentiment analysis result: label=%s, score=%f", label, score)
+        return {"label": label, "score": score}
+    except Exception as e:
+        logger.error("Error during sentiment analysis: %s", e)
+        return {"error": str(e)}
+
+# Add version endpoint
+@app.get("/version")
+def version():
+    return {"version": APP_VERSION}
+
+# Warm start NLP model
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Warming up sentiment analysis model...")
+    _ = await asyncio.to_thread(classifier, "This is a warm-up request")
+    logger.info("Sentiment analysis model warmed up successfully.")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        port=default_port,
+        host=default_host,
+        log_level=logging.INFO,
+        workers=1
+    )
--- test_main.py
+++ test_main.py
@@ -3,24 +3,44 @@ from main import app
 
 client = TestClient(app)
 
+HTTP_200_OK = 200
 
 def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-
+    try:
+        response = client.get("/")
+        assert response.status_code == HTTP_200_OK
+        assert response.json() == {"message": "Hello World"}
+    except AssertionError as e:
+        print(e)
+        assert False
+    except Exception as e:
+        print(e)
+        assert False
 
 def test_predict_positive():
-    response = client.post("/predict/",
+    try:
+        response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
-
+        json_data = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert json_data["label"] == "POSITIVE"
+    except AssertionError as e:
+        print(e)
+        assert False
+    except Exception as e:
+        print(e)
+        assert False
 
 def test_predict_negative():
-    response = client.post("/predict/",
+    try:
+        response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+        json_data = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert json_data["label"] == "NEGATIVE"
+    except AssertionError as e:
+        print(e)
+        assert False
+    except Exception as e:
+        print(e)
+        assert False
