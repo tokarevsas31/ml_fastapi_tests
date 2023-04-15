35dc59889dbaa2b327432db0041117171a73a1f3
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-04-15 10:46:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,23 @@ from transformers import pipeline
 from pydantic import BaseModel
 from typing import List
 
-class Items(BaseModel):
-    texts: List[str]
-
-@app.post("/predict_multiple/")
-def predict_multiple(items: Items):
-    return [classifier(text)[0] for text in items.texts]
-
 class Item(BaseModel):
     text: str
 
+class Items(BaseModel):
+    texts: List[str]
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+@app.post("/predict_multiple/")
+def predict_multiple(items: Items):
+    return [classifier(text)[0] for text in items.texts]
