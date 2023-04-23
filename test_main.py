43dd210846998a8cb729f8f56926e5681740301e
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-04-23 13:04:24 +0500

    Format code.

--- test_main.py
+++ test_main.py
@@ -6,6 +6,7 @@ client = TestClient(app)
 
 def test_read_main():
     """Test response from server for main endpoint. Resurn JSON object."""
+
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
@@ -13,6 +14,7 @@ def test_read_main():
 
 def test_predict_positive():
     """Test positive text prediction"""
+
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
     json_data = response.json()
@@ -22,6 +24,7 @@ def test_predict_positive():
 
 def test_predict_negative():
     """Test negative text prediction"""
+
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
