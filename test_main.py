bf4461b50e09ed7ee9992acf15b92c98cef31a2b
arqoofficial <argolubev97@yandex.ru>
2023-04-24 15:16:26 +0300

    update test_predict_neutral

--- test_main.py
+++ test_main.py
@@ -15,7 +15,7 @@ def test_predict_positive():
                            json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
+    assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative():
@@ -23,4 +23,11 @@ def test_predict_negative():
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+    assert json_data["label"] == "NEGATIVE"
+    
+def test_predict_neutral():
+    response = client.post("/predict/",
+                           json={"text": "Sky"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data["label"] == "NEUTRAL"
