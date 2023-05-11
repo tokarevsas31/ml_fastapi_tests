23d6be37c61ab68244f1a360386442224d0c1bc0
brain4power <a.gukov@escape-team.tech>
2023-05-11 12:10:17 +0300

    fix: remove unnecessary endpoint

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
--- test_main.py
+++ test_main.py
@@ -4,12 +4,6 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-
-
 def test_predict_positive():
     response = client.post("/predict/", json={"text": "I like machine learning!"})
     json_data = response.json()
