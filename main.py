20e5ea4a1c0acfef795cfee6ec6ac98b0a6d9fbf
Ivan Ivanov <101635841+vin-57@users.noreply.github.com>
2023-06-19 21:54:17 +0300

    Update main.py
     Added new method (predict2) and new class (Item2)

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from pydantic import BaseModel
 class Item(BaseModel):
     text: str
 
+class Item2(BaseModel):
+    text: str
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
 @app.post("/predict/")
 def predict(item: Item):
+    return classifier(item.text)[0]
+
+@app.post("/predict2/")
+def predict(item: Item2):
     return classifier(item.text)[0]
