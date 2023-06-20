5063a8e13bad22efb4bacce7ae2793e20a816690
Tatiana-Filimonova <virtroll@yandex.ru>
2023-06-20 21:02:37 +0300

    improve code style

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from pydantic import BaseModel
 
 
 class Item(BaseModel):
+
     text: str
 
 
 app = FastAPI()
+
 classifier = pipeline("sentiment-analysis")
 
 
 @app.get("/")
+
 def root():
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
+
 def predict(item: Item):
+    
     return classifier(item.text)[0]
