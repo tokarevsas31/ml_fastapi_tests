8818d672d4219a5ec2e926723faff78bcfa725fb
Maxim Lastin <lastinm@mail.ru>
2024-03-02 17:13:45 +0300

    Add power function

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ class Item(BaseModel):
     text: str
 
 
-
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-
 @app.get("/")
-def root():
+async def root():
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
-def predict(item: Item):
+async def predict(item: Item):
     return classifier(item.text)[0]
+
+
+@app.get("/power/")
+async def power(base: int = 1, exp: int = 1):
+    power = base ** exp
+    # power_str = str(base)
+    #             + " to the power of "
+    #             + str(exp)
+    #             + " is "
+    #             + str(power)
+    power_str = f"{base} to the power of {exp} is {power}."
+    return {"message": power_str}
