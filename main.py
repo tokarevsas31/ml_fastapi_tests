cbd3e061afebed9b45449d188f5c68c5b4374b99
SergeyChashnikov <chashserq@gmail.com>
2024-03-03 17:40:01 +0300

    add comment in main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+# Импорт библиотек
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
+# Создание класса
 class Item(BaseModel):
     text: str
 
-
-
+# Создаем классификатор для анализа настроений с помощью pipeline
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-
-
+# Обработчик маршрута для запросов по корневому пути
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
+# Возвращает результат обработки модели
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
