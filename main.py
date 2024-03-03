48165243399a3a8c3440dd9138701b9dd3859386
operevispa <perevispa@mail.ru>
2024-03-03 18:59:26 +0300

    add docstrings for documentation

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+    """Главная страница проекта. Выводит сообщение Hello world."""
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """API-метод позволяет передать методом POST в модель sentiment-analysis
+        текст и возвращает тональность переданного текста. """
     return classifier(item.text)[0]
