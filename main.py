370a910108dc48ab94c7d0e7de1fe1ed2c407959
Danila Moiseev <140072030+AnalystSurfer@users.noreply.github.com>
2024-03-03 18:39:04 +0300

    Update main.py
    Corrected code style according to pep8 + adding comments regarding the following code

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from transformers import pipeline
 from pydantic import BaseModel
 
 
+# Проверка типа данных
 class Item(BaseModel):
     text: str
 
 
-
+# Создание классификатора для анализа тональности текста
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-
+# Возвращает текст "Hello World"
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
+# Обработка POST-запроса и возвращение результата/ответа после обработки
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
