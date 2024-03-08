0d0e8683eb5adc772804f6e0aefc43b6fd320aef
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2024-03-08 14:31:05 +0500

    add data validation and error handling

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from transformers import pipeline
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
-    text: str
-
+    text: str = Field(default="I loved Star Wars so much!",
+                    title="Input text for sentiment analysis",
+                    max_length=300)
 
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
 
 
 @app.get("/")
@@ -18,6 +18,16 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.post("/predict/")
+@app.post("/predict/",
+          tags=["sentiment prediction"],
+          summary="This method utilizes a pre-trained sentiment analysis model to predict the sentiment of a text sample you privede.",
+          response_description="Prediction result"
+          )
 def predict(item: Item):
-    return classifier(item.text)[0]
+    if len(item.text)==0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    result = classifier(item.text)
+    return {"text": item.text, 
+            "label": result[0]['label'], 
+            "score": result[0]["score"]}
+
