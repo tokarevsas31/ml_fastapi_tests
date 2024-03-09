43cb9a380a204fa1039d02a34f6f77a471a1f8da
alexa313 <alex_313@mail.ru>
2024-03-09 17:01:15 +0000

    chore: initial fix code fomatting with black

--- test_main.py
+++ test_main.py
@@ -13,17 +13,15 @@ def test_read_main():
 
 def test_predict_positive():
     with TestClient(app) as client:
-        response = client.post("/predict/",
-                               json={"text": "I like machine learning!"})
+        response = client.post("/predict/", json={"text": "I like machine learning!"})
         json_data = response.json()
         assert response.status_code == 200
-        assert json_data['label'] == 'POSITIVE'
+        assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative():
     with TestClient(app) as client:
-        response = client.post("/predict/",
-                               json={"text": "I hate machine learning!"})
+        response = client.post("/predict/", json={"text": "I hate machine learning!"})
         json_data = response.json()
         assert response.status_code == 200
-        assert json_data['label'] == 'NEGATIVE'
+        assert json_data["label"] == "NEGATIVE"
