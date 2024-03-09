de4825ec480e23d800986fdce9cbb13ba6c839af
khit-mle <khit.mleng@gmail.com>
2024-03-09 10:28:18 +0000

    test: fix test_main.py to prevent sqlite table not found error

--- test_main.py
+++ test_main.py
@@ -5,22 +5,25 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World"}
 
 
 def test_predict_positive():
-    response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
+    with TestClient(app) as client:
+        response = client.post("/predict/",
+                               json={"text": "I like machine learning!"})
+        json_data = response.json()
+        assert response.status_code == 200
+        assert json_data['label'] == 'POSITIVE'
 
 
 def test_predict_negative():
-    response = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+    with TestClient(app) as client:
+        response = client.post("/predict/",
+                               json={"text": "I hate machine learning!"})
+        json_data = response.json()
+        assert response.status_code == 200
+        assert json_data['label'] == 'NEGATIVE'
