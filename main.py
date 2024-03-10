6a64f03bce0bd70a760d64a0de4d93c5e8ccfc9a
Vladimir <vvsergeev87@gmail.com>
2024-03-10 21:13:33 +0300

    Добавление докстринг для формаирования документации каждого метода в  модуле main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Модуль реализует API в приложении для классификации текста на предмет позитивного или негативного настроения.
+"""
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
@@ -15,9 +18,23 @@ classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+    """
+    Тестовое сообщение для проверки работоспособности"
+    :return: текстовое сообщение - приветсвие
+    """
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """
+    Выполянет классификации текста на предмет
+    позитивного или негативного настроения.
+    :param item: Текстовая фраза для оценки
+    :return: Возвращает словарь с результатом классификации текста.
+    В ключе:
+    "label" находится лейбл оценки, позитивная она или нет
+    "score" находится оценка вероятности
+    классификации текста этому лейблу
+    """
     return classifier(item.text)[0]
