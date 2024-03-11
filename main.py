d83f00c3d10587ab6af020b4a9ce09b837659490
Ivan Baklanov <nymless.ib@gmail.com>
2024-03-11 15:51:02 +0500

    Add data validation
    In this update, we have added:
    - Data validation to the Item model.
    - .gitignore file with __pycache__.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, validator
 from transformers import pipeline
-from pydantic import BaseModel
 
 
 class Item(BaseModel):
     text: str
 
+    @validator("text")
+    def text_length_check(cls, value):
+        if len(value) < 10:
+            raise ValueError("Text must be at least 10 characters")
+        return value
 
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
 
 
 @app.get("/")
