894abf233f9383ac3e290c1febcae5d9e31bbf92
user1 <vvorobiev@bk.ru>
2024-03-11 22:03:41 +0300

    add comments

--- test_main.py
+++ test_main.py
@@ -3,13 +3,13 @@ from main import app
 
 client = TestClient(app)
 
-
+# test1
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "World"}
 
-
+# test2
 def test_predict_positive():
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
@@ -17,7 +17,7 @@ def test_predict_positive():
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
-
+# test3
 def test_predict_negative():
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
