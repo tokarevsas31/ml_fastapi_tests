572bbd6d9ac399992c8ff783406dcb9347d21ac4
kavlab <kav.logrus@gmail.com>
2024-03-11 16:15:08 +0400

    add docstrings in main.py

--- main.py
+++ main.py
@@ -13,9 +13,27 @@ classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+    """Handler for root method. For test only.
+
+    Returns:
+        str: response in JSON format
+    """
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """Handler for /predict method.
+
+    Args:
+        item (Item): object with source text
+
+    Returns:
+        _type_: _description_
+    """
     return classifier(item.text)[0]
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8080)
