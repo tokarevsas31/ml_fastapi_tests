3d3351b7825ab9ceb9173742b5c6474d67e0e95c
Kryuchkov Vladislav <ppvel07@gmail.com>
2024-03-11 23:35:13 +0300

    Added a translation from English to Russian. Added a test for the English to Russian translation function.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from pydantic import BaseModel
 
 
@@ -7,10 +7,19 @@ class Item(BaseModel):
     text: str
 
 
-
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
 
+classifier = pipeline("sentiment-analysis")
+tokenizer_en_ru = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
+model_en_ru = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/"
+                                                    "opus-mt-en-ru")
+
+
+def translate_from_en(original_text):
+    inputs = tokenizer_en_ru(original_text, return_tensors="pt")
+    output = model_en_ru.generate(**inputs, max_new_tokens=100)
+    out_text = tokenizer_en_ru.batch_decode(output, skip_special_tokens=True)
+    return out_text[0]
 
 
 @app.get("/")
@@ -21,3 +30,9 @@ def root():
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+
+@app.post("/en-ru/")
+def translate_en_ru(item: Item):
+    orig_text = item.text
+    return translate_from_en(orig_text)
