b5bfb4bc20e77bc74108f4c9fcc79f7290154866
user1 <vvorobiev@bk.ru>
2024-03-12 22:43:23 +0300

    flake8 mistake is corrected

--- test_main.py
+++ test_main.py
@@ -3,11 +3,13 @@ from main import app
 
 client = TestClient(app)
 
+
 # тест 1
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
 
 # тест 2
 def test_predict_positive():
@@ -16,12 +18,14 @@ def test_predict_positive():
     assert response.status_code == 200
     assert json_data["Результат:"] == "позитивный :)"
 
+
 # тест 3
 def test_predict_negative():
     response = client.post("/predict/", json={"text": "I am very angry"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data["Результат:"] == "негативный (("
+
 
 # тест 4
 def test_predict_russian_positive():
@@ -30,12 +34,14 @@ def test_predict_russian_positive():
     assert response.status_code == 200
     assert json_data["Результат:"] == "позитивный :)"
 
+
 # тест 5
 def test_predict_russian_negative():
     response = client.post("/predict/", json={"text": "Я ненавижу тебя"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data["Результат:"] == "негативный (("
+
 
 # тест 6    
 def test_predict_russian_neutral():
@@ -44,14 +50,10 @@ def test_predict_russian_neutral():
     assert response.status_code == 200
     assert json_data["Результат:"] == "нейтральный"
 
+
 # тест 7
 def test_predict_neutral():
     response = client.post("/predict/", json={"text": "Today spring has come"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data["Результат:"] == "нейтральный"
-    
-    
-    
-    
-    
