f561b649874a8255bf32417a003d3158739ef70e
artempolozov <i@apolozov.ru>
2024-03-13 00:10:37 +0300

    Практика №2: - добавил новый api метод, выводящий статистику запросов текущей сессии: количество запросов (кроме root) и список всех запросов - добавил 2 теста: на 200 статус и проверку содержания получения статистики

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ class Item(BaseModel):
     text: str
 
 
+class HistoryData:
+    data: list
+
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
+history = HistoryData()
+history.data = list()
 
 
 @app.get("/")
@@ -20,4 +24,10 @@ def root():
 
 @app.post("/predict/")
 def predict(item: Item):
+    history.data.append(item.text)
     return classifier(item.text)[0]
+
+
+@app.get("/stats")
+def get_statistic():
+    return {len(history.data): history.data}
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from main import app, history
 
 client = TestClient(app)
 
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "World"}
+    assert response.json() == {"message": "Hello World"}
 
 
 def test_predict_positive():
@@ -24,3 +24,19 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+
+def test_get_statistic_healthy():
+    response = client.get("/stats")
+    assert response.status_code == 200
+
+
+def test_get_statistic_returns_correct_request_count_and_body():
+    history.data.clear()
+
+    client.post("/predict/", json={"text": "test11"})
+    client.post("/predict/", json={"text": "test22"})
+
+    response = client.get("/stats")
+    print(response)
+    assert response.json() == {"2": ["test11", "test22"]}
