54a339e7bc7a029c58ee5b1e215a2365890e69bf
Семён Каунов <kaunov.semyon@gmail.com>
2024-03-14 22:52:32 +0300

    main.py and README.md updated

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 # An example of ML Application with the pretrained model and test.
 
-An example of English text tone detection with [Hugging Face](https://huggingface.co/) library.
+An example of English text tone detection with [Hugging Face](https://huggingface.co/) library.  
 
+**Main function**
 
-Tests GitHub Actions
+Sentiment Analysis: Users can submit text for analysis, and the API returns the sentiment of the text using machine learning models.  
+
+**Local installation instructions**
+
+git clone git@github.com:tokarevsas31/ml_fastapi_tests.git  
+pip install -r requirements.txt  
+uvicorn main:app --reload  
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from transformers import pipeline
 from pydantic import BaseModel
-
 
 class Item(BaseModel):
     text: str
 
-
+def get_classifier():
+    return pipeline("sentiment-analysis")
 
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
-
-
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
 @app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+def predict(item: Item, classifier: pipeline = Depends(get_classifier)):
+    try:
+        result = classifier(item.text)[0]
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
