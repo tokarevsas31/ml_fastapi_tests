991b8d8ab95224b135e20161bc6c257f5db8e76f
Dmitry <savirm@yandex.ru>
2024-03-15 11:38:53 +0900

    Add logging to main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
+# Логирование
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Item(BaseModel):
     text: str
