eaecdb3d5988fed59011e0c234adbe595c81239e
Sergachev Yuri <98915877+SergachevYuri@users.noreply.github.com>
2024-03-15 17:33:24 +0300

    Обновленный стиль кода
    Убраны лишнии пкереходы строк.

--- main.py
+++ main.py
@@ -2,21 +2,15 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     text: str
-
-
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-
-
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
 
 @app.post("/predict/")
 def predict(item: Item):
