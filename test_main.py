30de070966f0d9cf37032bead6fd39ad3262aa15
Denis S <denis13th@gmail.com>
2024-03-17 20:16:04 +0300

    First commit

--- test_main.py
+++ test_main.py
@@ -24,3 +24,9 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+
+def test_get():
+    response = client.get("/predict/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Use POST + url to JPG image for recognition."}
