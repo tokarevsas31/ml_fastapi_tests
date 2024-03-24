df164a4f131d2ba03c6e737748403475a532faec
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-03-24 16:06:04 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,20 +7,20 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "World"}
+    assert response.json() == {"message": "Text analysis"}
 
 
-def test_predict_positive():
+def positive_prediction():
     response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
+                           json={"text": "I am a great fan of pizza"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
 
-def test_predict_negative():
+def negative_prediction():
     response = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
+                           json={"text": "I don't want to live in USA or europe"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
