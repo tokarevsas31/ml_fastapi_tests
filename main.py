fea9dda019b5fceef8be339ac2adf266ee8f550e
Aleksey Hokkonen <149786419+Antileha@users.noreply.github.com>
2024-03-24 18:30:04 +0300

    Update main.py
    Добавление документации в функции

--- main.py
+++ main.py
@@ -15,9 +15,24 @@ classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+     """
+    Обработчик для GET запроса на корневой URL.
+    
+    Returns:
+        dict: Словарь с сообщением "Hello World".
+    """
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """
+    Обработчик для POST запроса на /predict/.
+    
+    Args:
+        item (Item): Объект Item, содержащий текст для анализа настроения.
+        
+    Returns:
+        dict: Словарь с предсказанием настроения текста.
+    """
     return classifier(item.text)[0]
