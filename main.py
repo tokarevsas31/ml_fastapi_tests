61b455c6944ee0d258046ab033e8a2c13b95b15b
Alex48Yakunin <sanchezz.jakunin@rambler.ru>
2024-03-26 20:35:33 +0300

    добавлен новый функционал (см. описание к PR)
    - разработан новый метод API (predict_batch);
    - написан новый тест (test_predict_batch);
    - добавлена запись в файл requirements;

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
+from typing import List
 
 
 class Item(BaseModel):
@@ -19,3 +20,9 @@ def root():
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+
+@app.post("/predict/batch/")
+def predict_batch(items: List[Item]):
+    results = [classifier(item.text)[0] for item in items]
+    return results
