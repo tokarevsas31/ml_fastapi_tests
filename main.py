7c0b51884ab2303c808c95b7d00335b0ddaa4aaa
Ivan <ivanivanovplayer2019@gmail.com>
2024-03-27 00:01:26 +0100

    choose ML model

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Item(BaseModel):
 
 
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
+classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 
 
 @app.get("/model")
