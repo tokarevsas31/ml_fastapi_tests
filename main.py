ff3f443ad0ec82f1dba6b1e21d6bb0ec84069f14
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2024-03-27 22:34:50 +0500

    featured metha v3

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
+description = """
+PredictorApp API helps you predict the correct emotional color of the text. 🚀
 
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
 
 class Item(BaseModel):
     text: str
 
 
-app = FastAPI(
-    title="Predictor",
-    summary="This app will predict you the correct emotional color of the text (probaply)",
-    version="0.0.1",
-)
+app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
